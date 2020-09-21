a9ce13302e68ae15cd83ddc1a8cedeba916538e9
Will Bliss <willjbliss@gmail.com>
2020-09-21 17:34:50 -0500

    wow is bad

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ class Player(db.Model):
     id = db.Column(db.Integer, primary_key=True, index=True)
     slug = db.Column(db.String(120), unique=True)
     name = db.Column(db.String(120))
-    position = db.Column(db.String(2))
+    position = db.Column(db.String(10))
     shoots = db.Column(db.String(120))
     height = db.Column(db.String(4))
     weight = db.Column(db.Integer)
@@ -35,53 +35,33 @@ class SkaterSeason(db.Model):
     season = db.Column(db.String(7))
     age = db.Column(db.Integer)
     team = db.Column(db.String(3))
-    league = db.Column(db.String(4))
+    lg_id = db.Column(db.String(4))
     games_played = db.Column(db.Integer)
     goals = db.Column(db.Integer)
     assists = db.Column(db.Integer)
     points = db.Column(db.Integer)
-    penalty_minutes = db.Column(db.Integer)
-    even_strength_goals = db.Column(db.Integer)
-    power_play_goals = db.Column(db.Integer)
-    short_handed_goals = db.Column(db.Integer)
-    game_winning_goals = db.Column(db.Integer)
-    even_strength_assists = db.Column(db.Integer)
-    power_play_assists = db.Column(db.Integer)
-    short_handed_assists = db.Column(db.Integer)
+    plus_minus = db.Column(db.Integer)
+    pen_min = db.Column(db.Integer)
+    goals_ev = db.Column(db.Integer)
+    goals_pp = db.Column(db.Integer)
+    goals_sh = db.Column(db.Integer)
+    goals_gw = db.Column(db.Integer)
+    assists_ev = db.Column(db.Integer)
+    assists_pp = db.Column(db.Integer)
+    assists_sh = db.Column(db.Integer)
     shots = db.Column(db.Integer)
     shot_percentage = db.Column(db.Float)
     time_on_ice = db.Column(db.Integer)
-    average_time_on_ice = db.Column(db.Integer)
-    awards = db.Column(db.String)
+    time_on_ice_avg = db.Column(db.Float)
+    award_summary = db.Column(db.String)
     is_playoffs = db.Column(db.Boolean)
     won_cup = db.Column(db.Boolean)
 
-    def __init__(self, player_id, season, age, team, league, games_played, goals, assists, points, penalty_minutes, even_strength_goals, power_play_goals, short_handed_goals, game_winning_goals, even_strength_assists, power_play_assists, short_handed_assists, shots, shot_percentage, time_on_ice, average_time_on_ice, awards, is_playoffs, won_cup):
-
+    def __init__(self, player_id, season, team):
         self.player_id = player_id
         self.season = season
-        self.age = age
         self.team = team
-        self.league = league
-        self.games_played = games_played
-        self.goals = goals
-        self.assists = assists
-        self.points = points
-        self.penalty_minutes = penalty_minutes 
-        self.even_strength_goals = even_strength_goals
-        self.power_play_goals = power_play_goals
-        self.short_handed_goals = short_handed_goals
-        self.game_winning_goals = game_winning_goals
-        self.even_strength_assists = even_strength_assists
-        self.power_play_assists = power_play_goals
-        self.short_handed_assists = short_handed_assists
-        self.shots = shots
-        self.shot_percentage = shot_percentage
-        self.time_on_ice = time_on_ice
-        self.average_time_on_ice = average_time_on_ice
-        self.awards = awards
-        self.is_playoffs = is_playoffs
-        self.won_cup = won_cup
+
 
 class GoalieSeason(db.Model):
     __table_args__ = {'schema': 'hockey'}
--- run.py
+++ run.py
@@ -1,9 +1,33 @@
 from lxml import html
 import requests
 import dateparser
+import re
+import time
 
+from models import db, Player, SkaterSeason
 
-from models import db, Player
+SKATER_STATS = ['age', 
+                'lg_id', 
+                'games_played', 
+                'goals',
+                'assists', 
+                'points', 
+                'plus_minus', 
+                'pen_min', 
+                'goals_ev',
+                'goals_pp',
+                'goals_sh',
+                'goals_gw',
+                'assists_ev',
+                'assists_pp',
+                'assists_sh',
+                'shots',
+                'time_on_ice',
+                'time_on_ice_avg',
+                'award_summary'
+                ]
+
+ALPHABET = 'abcdefghijklmnopqrstuvwzyz'
 
 def run(base):
 
@@ -19,86 +43,137 @@ def run(base):
     # hit end
     # go to b
 
-    #repeat
+    # repeat
 
+def get_link_position(player):
+        
+    try: #  current players are bold
+        url = player.xpath('strong/a')[0].get('href')
+    except IndexError:
+        url = player.xpath('a')[0].get('href')
 
+    position = re.search("\,(.*?)\)", player.xpath('text()')[0]).group(1).strip() #  icky
+
+    return url, position
+
+def parse_letter(letter):
+
+    r = requests.get('https://www.hockey-reference.com/players/{}/'.format(letter))
+
+    tree = html.fromstring(r.content)
+    players = tree.xpath('//div[@id="div_players"]/p[@class="nhl"]')
+    
+    for player in players:
+        
+        url, position = get_link_position(player)
+        get_player_info(url, position)
+
+    db.session.commit()
 
 def get_player_info(url, position):
 
-    r = requests.get(url) # this will be determined later
+    r = requests.get('https://www.hockey-reference.com' + url)
 
     tree = html.fromstring(r.content)
     slug = None
-    name = tree.xpath('//h1[@itemprop="name"]/text()')[0]
+    name = tree.xpath('//h1[@itemprop="name"]/span/text()')[0]
 
-    ## below is a weird thing with 
-    # position is going to come from the site
-    shoots = tree.xpath('//*[@id="meta"]/div/p[1]/text()[2]')[0]
-    dob = dateparser.parse(tree.xpath('//*[@id="necro-birth"]/a/text()')[0] + ' ' + tree.xpath('//*[@id="necro-birth"]/a/text()')[1]) 
-    weight = tree.xpath('//*[@id="meta"]/div/p[2]/span[2]/text()')[0][:-2]
-    height = tree.xpath('//*[@id="meta"]/div/p[2]/span[1]/text()')[0]
+    try:
+        shoots = tree.xpath('//*[@id="meta"]/div/p[1]/text()[2]')[0][2]
+    except IndexError:
+        shoots = None
+
+    dob = dateparser.parse(tree.xpath('//*[@id="necro-birth"]/a/text()')[0] + ' ' + tree.xpath('//*[@id="necro-birth"]/a/text()')[1])
+    try:
+        weight = tree.xpath('//*[@id="meta"]/div/p/span[@itemprop="weight"]/text()')[0][:-2]
+    except IndexError:
+        weight = None
+    try:
+        height = tree.xpath('//*[@id="meta"]/div/p/span[@itemprop="height"]/text()')[0]
+    except IndexError:
+        height = None
     # nationality = tree.xpath('//*[@itemprop="birthPlace"]/a/@href') # regex needed to get url
-    is_hof = True if tree.xpath('//*[@class="important special"]/a/text()')[0] == 'Hall of Fame' else False
-
+    is_hof = True if 'Hall of Fame' in tree.xpath(
+        '//*[@class="important special"]/a/text()') else False
 
     player = Player(slug, name, position, shoots, height, weight, dob, is_hof)
 
     db.session.add(player)
+    db.session.flush()
+    if position != 'G':
+        total_stats = parse_skater(tree)
+        season = map_skater(player, total_stats)
+    time.sleep(4)
 
+def parse_skater(tree):
 
-def parse_skater(player_id):
-
-    seasons = tree.xpath('//table[@id="stats_basic_nhl"]/tbody/tr')
-    ids = []
+    seasons = tree.xpath('//table[@id="stats_basic_nhl"]/tbody/tr|//table[@id="partial_table"]/tbody/tr')
+    skater = {}
+    # each season object
     for season in seasons:
-        ids.append(season.value()[0])
+        season_id = season.values()[0]
+
+        try: 
+            season_year = season_id.split('.')[1]
+        except IndexError:
+            pass
+
+        # special logic for the team
+        # if the column is a total, then skip
+        try:
+            team = tree.xpath('//tr[@id="{}"]/td[@data-stat="team_id"]/a/text()'.format(season_id))[0]
+        except IndexError:
+            continue
+        # creates mapping structure
+        skater[season_year] = {}
+        skater[season_year][team] = {}
+        for stat in SKATER_STATS:
+            # these stats are links
+            if stat in ['lg_id', 'award_summary']:
+                try:
+                    skater[season_year][team][stat] = tree.xpath(
+                        '//tr[@id="{}"]/td[@data-stat="{}"]/a/text()'.format(season_id, stat))[0]
+                except IndexError:
+                    skater[season_year][team][stat] = None
+            else:
+                try:
+                    value = tree.xpath(
+                        '//tr[@id="{}"]/td[@data-stat="{}"]/text()'.format(season_id, stat))[0]
+                    
+                    # convert avg toi from mm:ss to float
+                    if stat == 'time_on_ice_avg':
+                        minutes, seconds = value.split(':')
+                        seconds = int(seconds) / 60
+                        value = int(minutes) + seconds
+
+                    skater[season_year][team][stat] = value
+                        
+                except IndexError:
+                    skater[season_year][team][stat] = None
+
+    return(skater)
 
 
-    for id in ids:
-        
-        season = id.split('.')[1]
-        age = tree.xpath('//tr[@id="{}"]/td[@data-stat="age"]/text()'.format(id))
-
+def map_skater(player, total_stats):
     
+    season = None
+    for year, team_stats in total_stats.items():
+        for team, stats in team_stats.items():
+            season = SkaterSeason(player.id, year, team)
+            for stat, value in stats.items():
+                setattr(season, stat, value)
+            db.session.add(season)
+            
+
+
+
+
 """db.drop_all()
     db.create_all()
     get_player_info('https://www.hockey-reference.com/players/a/appssy01.html', 'C')
     db.session.commit()"""
 
 if __name__ == '__main__':
-   
-    """
-    seasons = tree.xpath('//table[@id="stats_basic_nhl"]/tbody/tr')
-    print("foo")"""
-    
-    r = requests.get('https://www.hockey-reference.com/players/a/appssy01.html') # this will be determined later
 
-    tree = html.fromstring(r.content)
-    
-    seasons = tree.xpath('//table[@id="stats_basic_nhl"]/tbody/tr')
-    stats = ['age', 'pen_min']
-    ids = []
-    for season in seasons:
-        ids.append(season.values()[0])
-    skater_season = {}
-    for id in ids:
-        
-        season = id.split('.')[1]
-        skater_season[season] = {}
-        for stat in stats:
-            skater_season[season][stat] = tree.xpath('//tr[@id="{}"]/td[@data-stat="{}"]/text()'.format(id, stat))[0]
-
-    print(skater_season)
-        
-
-
-
-
-    
-
-
-
-    
-
-
-
+    for letter in ALPHABET:
+        parse_letter(letter)
